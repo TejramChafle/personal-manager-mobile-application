a9e3b73115a27e4242c5d6be4274c57fa63d5059
Tejram <tejram_chafle@yahoo.in>
2020-09-11 12:13:53 +0530

    Added config.ts for firebase config

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDrF3MrUSxyKzSCnHZjWy8gcynUUxknr_g",
+  authDomain: "ng-personal-manager.firebaseapp.com",
+  databaseURL: "https://ng-personal-manager.firebaseio.com",
+  projectId: "ng-personal-manager",
+  storageBucket: "ng-personal-manager.appspot.com",
+  messagingSenderId: "1072996084283",
+  appId: "1:1072996084283:web:7f4aa09a1f540309008a1e",
+  measurementId: "G-ER0C9LMQVZ"
+};
