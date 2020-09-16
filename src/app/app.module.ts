f0708fa37c25296f929ac15cd0d5f3971f2f5e91
Tejram <tejram_chafle@yahoo.in>
2020-09-16 23:02:36 +0530

    Added ionic device plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LauncherPageModule } from '../pages/launcher/launcher.module';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Push } from '@ionic-native/push';
+import { Device } from '@ionic-native/device';
 
 // Service Providers
 import { AppProvider } from './app.provider';
@@ -55,6 +56,7 @@ import { AuthInterceptor } from '../interceptors/auth.interceptor';
     StatusBar,
     SplashScreen,
     Push,
+    Device,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     {
       provide: HTTP_INTERCEPTORS,
